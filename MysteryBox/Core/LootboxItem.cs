37ee49b6b212e9bb6e99e9b9800b95050ff52c64
thesuicideheart <tillolgamer@gmail.com>
2018-11-22 15:19:52 +0100

    owo

---
+++ MysteryBox/Core/LootboxItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysteryBox.Core
+{
+    public class LootboxItem
+    {
+        public int MinNum;
+        public int MaxNum;
+        public string ItemId;
+
+        public int Range {
+            get
+            {
+                return MaxNum - MinNum;
+            }
+        }
+
+        public LootboxItem(string id, int min,int max)
+        {
+            ItemId = id;
+            MinNum = min;
+            MaxNum = max;
+        }
+
+
+
+    }
+}
