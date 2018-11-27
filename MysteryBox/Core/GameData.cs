b64597e7776c0ccad08504028d1e5b9f1e83d655
thesuicideheart <tillolgamer@gmail.com>
2018-11-27 13:08:41 +0100

    Added all the tiered wands

--- MysteryBox/Core/GameData.cs
+++ MysteryBox/Core/GameData.cs
@@ -59,9 +59,9 @@ namespace MysteryBox.Core
             AddItem(new Item("t13_dagger", "Dagger of Sinister Deeds", 650, Rarity.Legendary, "Dagger of Sinister Deeds"));
 
             #endregion
-
-            #region katanas
             
+            #region katanas
+
             AddItem(new Item("t0_katana", "Rusty Katana", 5, Rarity.Common, "Rusty Katana"));
             AddItem(new Item("t1_katana", "kendo Stick", 7, Rarity.Common, "kendo Stick"));
             AddItem(new Item("t2_katana", "Plain Katana", 10, Rarity.Common, "Plain Katana"));
@@ -78,6 +78,27 @@ namespace MysteryBox.Core
             AddItem(new Item("t13_katana", "Sadamune", 650, Rarity.Legendary, "Sadamune"));
 
             #endregion
+
+            #region wands
+
+            AddItem(new Item("t0_wand", "Fire Wand", 5, Rarity.Common, "Fire Wand"));
+            AddItem(new Item("t1_wand", "Force Wand", 7, Rarity.Common, "Force Wand"));
+            AddItem(new Item("t2_wand", "Power Wand", 10, Rarity.Common, "Power Wand"));
+            AddItem(new Item("t3_wand", "Missile Wand", 15, Rarity.Common, "Missile Wand"));
+            AddItem(new Item("t4_wand", "Eldritch Wand", 22, Rarity.Common, "Eldritch Wand"));
+            AddItem(new Item("t5_wand", "Hell’s Fire Wand", 32, Rarity.Common, "Hell’s Fire Wand"));
+            AddItem(new Item("t6_wand", "Wand of Dark Magic", 47, Rarity.Common, "Wand of Dark Magic"));
+            AddItem(new Item("t7_wand", "Wand of Arcane Flame", 70, Rarity.Common, "Wand of Arcane Flame"));
+            AddItem(new Item("t8_wand", "Wand of Death", 100, Rarity.Common, "Wand of Death"));
+            AddItem(new Item("t9_wand", "Wand of Deep Sorcery", 145, Rarity.Common, "Wand of Deep Sorcery"));
+            AddItem(new Item("t10_wand", "Wand of Shadow", 212, Rarity.Uncommon, "Wand of Shadow"));
+            AddItem(new Item("t11_wand", "Wand of Ancient Warning", 310, Rarity.Uncommon, "Wand of Ancient Warning"));
+            AddItem(new Item("t12_wand", "Wand of Recompense", 450, Rarity.Rare, "Wand of Recompense"));
+            AddItem(new Item("t13_wand", "Wand of Evocation", 650, Rarity.Legendary, "Wand of Evocation"));
+
+            #endregion
+
+
 
         }
 
