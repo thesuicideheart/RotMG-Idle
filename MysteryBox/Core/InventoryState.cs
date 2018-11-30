0a60370fd93152b34663f960d39753c0b8c6c93e
thesuicideheart <tillolgamer@gmail.com>
2018-12-01 00:48:19 +0100

    Added "current selected item" rendering to inventory

--- MysteryBox/Core/InventoryState.cs
+++ MysteryBox/Core/InventoryState.cs
@@ -15,7 +15,7 @@ namespace MysteryBox.Core
         public Player Player;
 
         private int invX = 60;
-        private int invY = Option.Height / 2;
+        private int invY = Option.Height / 2 - 40;
         private int invWidth = 512;
         private int invHeight = 38;
         private int invListCenterX = 0;//invX + 171;
@@ -24,7 +24,12 @@ namespace MysteryBox.Core
 
         private int SelectedItem = 0;
 
-        private Texture2D invBackground;
+        private Texture2D invBackground, itemSlot;
+
+        private Rectangle itemSlotRect,
+            itemNameRect,
+            itemRarityRect,
+            itemPriceRect;
 
         public InventoryState(Player player) : base(GameData.InvState)
         {
@@ -57,15 +62,24 @@ namespace MysteryBox.Core
             invListCenterX = invX + 44;
 
             invBackground = Sprites.GetTexture("inventory");
+            itemSlot = Sprites.GetTexture("item_slot");
+            itemSlotRect = new Rectangle(Option.Width / 2 - (itemSlot.Width / 2), Option.Height / 2 - 200, itemSlot.Width, itemSlot.Height);
+
+            itemNameRect = new Rectangle(itemSlotRect.X - 96, itemSlotRect.Y + itemSlotRect.Height + 10, 256, 48);
+            itemRarityRect = new Rectangle(itemNameRect.X, itemNameRect.Y + itemNameRect.Height + 10, 256, 24);
+            itemPriceRect = new Rectangle(itemRarityRect.X, itemRarityRect.Y + itemRarityRect.Height + 10, 256, 24);
+
 
         }
 
         public override void Draw(SpriteBatch batch)
         {
 
-            batch.Draw(invBackground, new Rectangle(0,0, Option.Width,Option.Height), Color.White);
+            batch.Draw(invBackground, new Rectangle(0, 0, Option.Width, Option.Height), Color.White);
+            batch.Draw(itemSlot, itemSlotRect, Color.White);
 
-            /*for (int i = -10; i < 11; i++)
+
+            for (int i = -3; i < 4; i++)
             {
                 if (SelectedItem + i < 0 || SelectedItem + i >= Player.Inventory.Count) continue;
                 var iItem = Player.Inventory[SelectedItem + i];
@@ -73,15 +87,18 @@ namespace MysteryBox.Core
 
                 if (i == 0)
                 {
-                    Game1.Instance.draw(item.GetTexture(), new Rectangle(invX - item.GetTexture().Width, (invY + i * invListSpacing) + 10, item.GetTexture().Width, item.GetTexture().Height));
-                    Game1.Instance.drawString($">{item.Name} x{iItem.Count}<", invListCenterX, invListCenterY + i * invListSpacing);
+                    Game1.Instance.draw(item.GetTexture(), new Rectangle(invX - item.GetTexture().Width - 10, (invY + i * invListSpacing) - 15, item.GetTexture().Width * 2, item.GetTexture().Height * 2));
+                    Game1.Instance.draw(item.GetTexture(), new Rectangle(itemSlotRect.X + 7, itemSlotRect.Y + 7, item.GetTexture().Width, item.GetTexture().Height));
+                    Utils.DrawSmallString(item.Name, itemNameRect, Color.White);
+                    Utils.DrawRarityString(item, itemRarityRect);
+                    Utils.DrawSmallString($"Price: {item.Price} fame", itemPriceRect, Color.White);
+                    //Game1.Instance.drawString($">{item.Name} x{iItem.Count}<", invListCenterX, invListCenterY + i * invListSpacing);
                 }
                 else
                 {
-                    Game1.Instance.drawString($"{item.Name} x{iItem.Count}", invListCenterX, invListCenterY + i * invListSpacing);
                     Game1.Instance.draw(item.GetTexture(), new Rectangle(invX - item.GetTexture().Width, (invY + i * invListSpacing) + 10, item.GetTexture().Width, item.GetTexture().Height));
                 }
-            }*/
+            }
 
             base.Draw(batch);
         }
